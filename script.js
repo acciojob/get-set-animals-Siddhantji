d4d5fef2a4d3c148b07a24e53cadcca9da8dc9eb
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-01 19:14:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Animal {}
+class Animal {
+	cosntructor(species){
+		this._species= species;
+	}
+	get species(){
+		return this._species;
+	}
+	makeSound(){
+		console.log();
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species){
+		super(species);
+	}
+	bark(){
+		console.log('woof');
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(species){
+		super(species);
+	}
+	purr(){
+		console.log('purr');
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
